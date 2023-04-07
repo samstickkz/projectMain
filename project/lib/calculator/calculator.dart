29dbb495c1b97a452214cfea1e3caf8e0519819d
samuel joseph (samstickkz) <39702515+samstickkz@users.noreply.github.com>
2023-04-07 22:16:27 +0100

    podfile fixes

--- project/lib/calculator/calculator.dart
+++ project/lib/calculator/calculator.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
-
 import 'package:math_expressions/math_expressions.dart';
-
+import 'package:flutter/material.dart';
+import '../screens/nav.dart';
 import 'buttons.dart';
 
 class CalculatorPage extends StatefulWidget {
@@ -13,6 +12,8 @@ class CalculatorPage extends StatefulWidget {
 
 var userQuestion = '';
 var userAnswer = '';
+
+
 
 final List<String> buttons = [
   'C',
@@ -66,92 +67,118 @@ class _CalculatorPageState extends State<CalculatorPage> {
         child: Column(
           children: [
             Expanded(
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'BTC: \$26,000 ',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const NavPage(),
+                                  ),
+                                );
+                              },
+                              child: const Icon(
+                                Icons.arrow_back,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const Text(
+                              'BTC: \$26,000 ',
+                              style: TextStyle(
+                                color: Colors.orange,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Container(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          userQuestion,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Container(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            userQuestion,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 20.0, right: 8),
-                      child: Container(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          '= $userAnswer',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0, right: 8),
+                    child: Container(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        '= $userAnswer',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
             Expanded(
               flex: 2,
-              child: Container(
-                  child: GridView.builder(
-                      itemCount: buttons.length,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 4),
-                      itemBuilder: (BuildContext context, int index) {
-                        return Buttons(
-                          buttontapped: () {
-                            setState(() {
-                              if (buttons[index] == 'DEL') {
-                                // Delete last item added
-                                userQuestion = userQuestion.substring(
-                                    0, userQuestion.length - 1);
-                              } else if (buttons[index] == 'C') {
-                                // Clear everything
-                                userQuestion = '';
-                                userAnswer = '';
-                              } else if (buttons[index] == '=') {
-                                // Evaluate userQuestion
-                                equalPressed();
-                                if (userQuestion.isNotEmpty) {
-                                  // Evaluate expression
-                                  // ...
-                                  // Set userAnswer to result of evaluation
-                                } else {
-                                  userAnswer = 'Clear';
-                                }
-                              } else {
-                                userQuestion += buttons[index];
-                              }
-                            });
-                          },
-                          color: isOperator(buttons[index])
-                              ? Colors.deepPurpleAccent
-                              : isBtn(buttons[index])
-                                  ? Colors
-                                      .green // Set color to green if button is 'News'
-                                  : Colors.deepPurpleAccent.shade100,
-                          textColor: Colors.white,
-                          buttonText: buttons[index],
-                        );
-                      })),
+              child: GridView.builder(
+                  itemCount: buttons.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Buttons(
+                      buttontapped: () {
+                        setState(() {
+                          if (buttons[index] == 'DEL') {
+                            // Delete last item added
+                            userQuestion = userQuestion.substring(
+                                0, userQuestion.length - 1);
+                          } else if (buttons[index] == 'C') {
+                            // Clear everything
+                            userQuestion = '';
+                            userAnswer = '';
+                          } else if (buttons[index] == '=') {
+                            // Evaluate userQuestion
+                            equalPressed();
+                            if (userQuestion.isNotEmpty) {
+                              // Evaluate expression
+                              // ...
+                              // Set userAnswer to result of evaluation
+                            } else {
+                              userAnswer = 'Clear';
+                            }
+                          } else {
+                            userQuestion += buttons[index];
+                          }
+                        });
+                      },
+                      color: isOperator(buttons[index])
+                          ? Colors.deepPurpleAccent
+                          : isBtn(buttons[index])
+                              ? Colors
+                                  .green // Set color to green if button is 'News'
+                              : Colors.deepPurpleAccent.shade100,
+                      textColor: Colors.white,
+                      buttonText: buttons[index],
+                    );
+                  }),
             ),
           ],
         ),
