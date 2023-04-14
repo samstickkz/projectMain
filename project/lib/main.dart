e9cb068e258002a85c137fe9bb49a300a4bddc02
samuel joseph (samstickkz) <39702515+samstickkz@users.noreply.github.com>
2023-04-14 19:09:59 +0100

    Getx

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:project/onboarding/main_onboarding.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       theme: ThemeData(
           appBarTheme: AppBarTheme(
         elevation: 0,
